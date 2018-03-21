77f6d2b409c3ecff491b7d9d2ef3709d9c20b5e6
yvadher <yvadher@asb9812u-a02.csil.sfu.ca>
2018-03-21 01:44:16 -0700

    First commit

---
+++ prj4/array_stats.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+
+#include "array_stats.h"
+
+asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long size){
+
+	struct array_stats temp = {0, 0, 0};
+	long data = 0;
+	long result = 0;
+	int i = 0;
+
+	while (i<size)
+	{
+
+		if (copy_from_user(&data, &data[i], sizeof(data[i])))
+		{
+			return -EFAULT;
+		} 		
+
+		//Set the min max values to the first element
+		if (i == 0){
+			temp.min = data;
+			temp.max = data;
+		}
+
+		if (temp.min > data)
+		{
+			temp.min = data;
+		}
+		if (temp.max < data)
+		{
+			temp.max = data;
+		}
+
+		temp.sum = (temp.sum + data);
+		
+		i++;
+	}
+
+	if (copy_to_user(stats, &temp, sizeof(temp)))
+	{
+		return -EFAULT;
+	}
+	return result;
+
+
+
+}
