326a8f2dbb763c3a0f04ee98c718707c7e1bf778
yvadher <yagniktechmail@gmail.com>
2018-03-22 12:24:39 -0700

    Add a print messages

--- prj4/ProcessAncestor/process_ancestors.c
+++ prj4/ProcessAncestor/process_ancestors.c
@@ -30,17 +30,18 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[],long size
 	int i=0;
 	int j=0;	
 
-
-   
-
 	for (task = current; task != &init_task; task = task->parent) {
 		
 		process.pid = (long)task->pid; 
-	
+		printk("Process ID: %ld\n", process.pid);	
+
 		process.state = task->state;
-		
+		printk("Process State: %ld\n", process.state);		
+
 		memset(process.name, '\0', sizeof(process.name));
 		strcpy(process.name, task->comm);
+
+		printk("Process Name: %ld\n", process.name);
 		
 
 		list_for_each(list, &current->children) {
@@ -49,12 +50,23 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[],long size
 		list_for_each(list, &current->sibling) {
 			count_sibling++;
 		}
-		process.num_children = countChildren;
-		process.num_siblings = count_sibling;		
+		process.num_children = ;
+		process.num_siblings = count_sibling;	
+
+		printk("Process numChildren: %ld\n", process.countChildren);			
 		
 		info_array[i] = process;
+		
+		if(i == size){
+			*num_filled = i;
+			printk("Process numFilled: %ld\n", *num_filled);	
+			return 0;		
+		}
+
 		i++;
 	}
+	*num_filled = i;
+	printk("Process numFilled: %ld\n", *num_filled);	
 
 	return 0;
-}
+}countChildren
