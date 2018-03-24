84ce34fe040d403900a1db40fc75569b797059d9
yvadher <yagniktechmail@gmail.com>
2018-03-24 15:40:41 -0700

    Bug fix

--- prj4/ProcessAncestor/process_ancestors.c
+++ prj4/ProcessAncestor/process_ancestors.c
@@ -4,6 +4,8 @@
 #include <linux/sched.h>
 #include <linux/cred.h>
 #include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 
 asmlinkage long sys_process_ancestors(struct process_info info_array[],long size,long *num_filled) ;
@@ -12,6 +14,8 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[],long size
 struct process_info get_process_info(struct task_struct* this) {
     struct process_info result;
     struct list_head* ptr;
+    int count_children = 0;
+    int count_sibalings = 0;
 
     /* Assign trivial info. */
     result.pid = this->pid;
@@ -21,46 +25,46 @@ struct process_info get_process_info(struct task_struct* this) {
     result.nvcsw = this->nvcsw;
     result.nivcsw = this->nivcsw;
 
-	int count_children = 0;
+	
     list_for_each(ptr, &(this->children)) {
     	++count_children;
     }
-
-	int count_sibalings = 0;
+	
     list_for_each(ptr, &(this->sibling)) {
     	++count_sibalings;
     }
 
     //Assign childrens and sibalings
-    result.num_children = temp_num_children;
-    result.num_siblings = temp_num_sibling;
+    result.num_children = count_children;
+    result.num_siblings = count_sibalings;
 
-    return res;
+    return result;
 }
 
 asmlinkage long sys_process_ancestors(struct process_info info_array[],long size,long *num_filled) {
-
-	//Error checks
-    if (size<=0) return -EINVAL;
-	if (!info_array || !num_filled) return -EFAULT;
-	 
-	int filled_processes= 0;
+	int i;
+	long filled_processes= 0;
 	struct task_struct *task = current; 
 	struct task_struct *prev;
+	struct process_info *info_array_temp;
 
-	struct process_info info_array_temp;
+    	//Error checks
+    	if (size<=0) return -EINVAL;
+	if (!info_array || !num_filled) return -EFAULT;
+	 
+	
 	// Allocate memory
-    info_array_temp = kmalloc(sizeof (struct process_info) * size, GFP_KERNEL);
+    	info_array_temp = kmalloc(sizeof (struct process_info) * size, GFP_KERNEL);
 	
 	// Copy info_array from user to kernal space
-	int i;
-    for (i = 0; i < size; i++) {
-        if (copy_from_user(&info_array_temp[i], &info_array[i], sizeof (struct process_info))) {
-            kfree(info_array_temp);
-			printk("Safe memory violation in porcess ancestors. Cant copy the memory from user to kernal.\n");
-            return -EFAULT;
-        }
-    }
+	
+	for (i = 0; i < size; i++) {
+		if (copy_from_user(&info_array_temp[i], &info_array[i], sizeof (struct process_info))) {
+		    kfree(info_array_temp);
+				printk("Safe memory violation in porcess ancestors. Cant copy the memory from user to kernal.\n");
+		    return -EFAULT;
+		}
+	}
 
 	// fill info_array_temp with irrerating to processes
 	i = 0;
@@ -75,20 +79,20 @@ asmlinkage long sys_process_ancestors(struct process_info info_array[],long size
 
 	//Copy info_array_temp to user space
 	for (i = 0; i < filled_processes; i++) {
-        if (copy_to_user(&info_array[i], &info_array_temp[i], sizeof (struct process_info))) {
-            kfree(info_array_temp);
-			printk("Safe memory violation! Cant copy data to user space!");
-            return -EFAULT;
-        }
-    }
+		if (copy_to_user(&info_array[i], &info_array_temp[i], sizeof (struct process_info))) {
+		    kfree(info_array_temp);
+				printk("Safe memory violation! Cant copy data to user space!");
+		    return -EFAULT;
+		}
+	}
 
-	/* Copy back the num_filed number with -EFAULT checks. */
-    if (copy_to_user(num_filled, filled_processes, sizeof (long))) {
+	// Copy  the num_filed number
+	if (copy_to_user(num_filled, &filled_processes, sizeof (long))) {
 		kfree(info_array_temp);
 		printk("Safe memory violation! Cant copy data to user space!");
-        return -EFAULT;
-    }
-
+		return -EFAULT;
+	}
+	
 	kfree(info_array_temp);
 	return 0;
 }
